fe5f14e7e234c5ed9554dc6e9f0beb81d4faa4a2
unknown <SftwDude@outlook.com>
2020-05-03 12:39:40 -0400

    Add Active section

--- js/app.js
+++ js/app.js
@@ -51,7 +51,8 @@ function initialize() {
     //create a container for all the sections
     const div = document.createElement('div');
     div.id = 'sectionContainer';
-    
+    div.addEventListener('scroll', makeActive, false);
+
     let parentNode;
     for (let i = 0; i < sections.length; i++) {
         sectionInfo = new SectionInfo(sections[i].id);
@@ -63,9 +64,6 @@ function initialize() {
             //get the parent of the first section
             parentNode = sections[0].parentNode;
             parentNode.appendChild(div);
-
-            var node = document.querySelector('#sectionContainer');
-            node.addEventListener('scroll',makeActive,false);
         }
         Sections.push(sectionInfo);
 
@@ -93,8 +91,63 @@ function buildNavigation() {
 
 // Add class 'active' to section when near top of viewport
 function makeActive() {
-    console.log("scrolling...");
+    let activeSection = null;
+
+    //the sections are scrolling with in the container. Check the sections's relation
+    //with the container to figure out which section should be active
+    Sections.forEach(sectionInfo => {
+        const container = document.getElementById('sectionContainer');
+        var cTop = container.scrollTop;
+        var cBottom = cTop + container.clientHeight;
+        const containerRect = container.getBoundingClientRect();
+
+        var px = window.getComputedStyle(container, null).getPropertyValue('font-size');
+
+        const section = document.getElementById(`${sectionInfo.Id}`);
+        var offsetparent = section.offsetParent;
+
+        const sectionRect = section.getBoundingClientRect();
+        var eTop = section.offsetTop;
+        var eBottom = eTop + section.clientHeight;
+        //console.log(`section ${sectionInfo.data} c.top:${containerRect.top},c.bottom:${containerRect.bottom} s.top:${sectionRect.top},s.bottom:${sectionRect.bottom},s.height:${sectionRect.height}`);
+        //console.log(`s.height - s.top = ${sectionRect.height - sectionRect.top}`)
+
+        const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+        //const verticalInView = (sectionRect.top <= windowHeight) && ((sectionRect.top + sectionRect.height) >=0);
+        console.log(`section ${sectionInfo.Id} cTop:${cTop},cBottom:${cBottom} eTop:${eTop},eBottom:${eBottom}`);
+        //const verticalInView = ((eTop < cTop) && (eBottom > cTop)) ||
+        //((eBottom > cBottom) && (eTop < cBottom))
+        const verticalInView = (eBottom <= cBottom);
+        if (verticalInView) {
+            console.log(`section ${sectionInfo.Id} is ACTIVE`);
+            activeSection = sectionInfo.Id;
+        }
+    });
+    console.log(`section ${activeSection} is ACTIVE`);
+    if (activeSection !== null) {
+        Sections.forEach(sectionInfo => {
+            if (sectionInfo.Id == activeSection) {
+                if (!sectionInfo.active) {
+                    sectionInfo.active = true;
+
+                    const section = document.getElementById(`${sectionInfo.Id}`);
+                    section.classList.add("your-active-class");
+                }
+            }
+            else {
+                const section = document.getElementById(`${sectionInfo.Id}`);
+                if (sectionInfo.active) {
+                    section.classList.remove("your-active-class");
+                }
+
+                sectionInfo.active = false;
+
+            }
+        });
+    }
+
 }
+
 
 // Scroll to anchor ID using scrollTO event
 
